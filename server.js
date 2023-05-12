ffdc01b4c815daf1e943c884e5a248ddad52fd60
ADEVA SATRIA ARIF WIBAWA <adeva.wibawa@binus.ac.id>
2023-05-12 22:26:17 +0700

    Berhasil Melakukan Fetch data or get data from database

--- server.js
+++ server.js
@@ -4,6 +4,10 @@
     2. Berhasil melakukan penginsatalan modul dengan npm express 
     3. Berhasil melakukan penginsatalan modul dengan Nodemon 
     4. Behasil melakukan Conncet dengan mongo DB dengan menggunakan Mongoose
+    5. Berhasil memasukkan model schema
+    6. Berhasil melakukan test post Json product & Masuk dalam MongoDB
+    7. [] Fetch data or get data from Database
+
 */ 
 
 const Product = require('./Models/productmodels');
@@ -24,7 +28,26 @@ app.get('/blog', (req, res)=>{
     res.send('Hello Blog My name is adeva ');
 });
 
-app.post('/product', async(req, res) =>{
+app.get('/products', async(req, res)=>{
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products)
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+})
+
+app.get('/products/:id', async(req, res)=>{
+    try {
+        const {id} = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product)
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+})
+
+app.post('/products', async(req, res) =>{
     try {
         const product = await Product.create(req.body)
         res.status(200).json(product);
